85996a219c846873fe708873d3bc69e8eaa417d8
Antonio Neto <antonio.silva6@yahoo.com.br>
2020-04-22 12:05:07 -0300

    Criei o projeto

---
+++ Credit-Suisse/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Credit_Suisse.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
