d5fa8091f5692f191b77f3a7f9c787ec2a40ccea
Antonio Neto <antonio.silva6@yahoo.com.br>
2020-04-23 10:14:01 -0300

    Criei repositório padrão

---
+++ src/DevIO.Api/Controllers/MainController.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Linq;
+using DevIO.Business.Intefaces;
+using DevIO.Business.Notificacoes;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+
+namespace DevIO.Api.Controllers
+{
+    [ApiController]
+    public abstract class MainController : ControllerBase
+    {
+        private readonly INotifier _notifier;
+
+        protected MainController(INotifier notifier)
+        {
+            _notifier = notifier;
+        }
+
+        protected bool ValidOperation()
+        {
+            return !_notifier.HasNotification();
+        }
+
+        protected ActionResult CustomResponse(object result = null)
+        {
+            if (ValidOperation())
+            {
+                return Ok(new
+                {
+                    success = true,
+                    data = result
+                });
+            }
+
+            return BadRequest(new
+            {
+                success = false,
+                errors = _notifier.GetNotifications().Select(n => n.Message)
+            });
+        }
+
+        protected ActionResult CustomResponse(ModelStateDictionary modelState)
+        {
+            if(!modelState.IsValid) NotifyModelErrorInvalid(modelState);
+            return CustomResponse();
+        }
+
+        protected void NotifyModelErrorInvalid(ModelStateDictionary modelState)
+        {
+            var errors = modelState.Values.SelectMany(e => e.Errors);
+            foreach (var error in errors)
+            {
+                var errorMsg = error.Exception == null ? error.ErrorMessage : error.Exception.Message;
+                ReportError(errorMsg);
+            }
+        }
+
+        protected void ReportError(string message)
+        {
+            _notifier.Handle(new Notification(message));
+        }
+    }
+}
---
+++ src/DevIO.Business/Intefaces/INotifier.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+using DevIO.Business.Notificacoes;
+
+namespace DevIO.Business.Intefaces
+{
+    public interface INotifier
+    {
+        bool HasNotification();
+        List<Notification> GetNotifications();
+        void Handle(Notification notification);
+    }
+}
